194391752cfdf146ad8a5085b38602f8cffca6ae
Peter Levinsky <pele@zealand.dk>
2024-10-21 13:55:12 +0200

    Add project files.

---
+++ ClassDemoDesignPattern/pattern/BehavioralPattern/observer/ObservableObject.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ClassDemoDesignPatterns.pattern.BehavioralPattern.observer
+{
+    class ObservableObject:INotifyPropertyChanged
+    {
+        private int _id;
+        private String _text;
+
+
+        public ObservableObject(int id, string text)
+        {
+            _id = id;
+            _text = text;
+        }
+
+        public ObservableObject():this(-1,"dummy")
+        {
+        }
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value == _id) return;
+                _id = value;
+                Notify("Id");
+            }
+        }
+
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (value == _text) return;
+                _text = value;
+                Notify("Text");
+            }
+        }
+
+
+        /*
+         * Observer
+         */
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        public void Notify(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Text)}: {Text}";
+        }
+
+    }
+}
---
+++ ClassDemoDesignPattern/pattern/CreationalPattern/singleton/NoteCatalogue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassDemoDesignPatterns.pattern.CreationalPattern.singleton
+{
+    class NoteCatalogue
+    {
+        /*
+         * Singleton
+         */
+        // trin 1
+        private NoteCatalogue()
+        {
+            this.notes = new List<string>()
+            {
+                "basic note1", "basic note2", "basic note3"
+            };
+        }
+
+        // trin 2
+        //private static NoteCatalogue _instance = new NoteCatalogue();  // eager
+        private static NoteCatalogue _instance = null; // lazy
+
+        // trin 3
+        //public static NoteCatalogue Instance => _instance; // eager
+
+        private static object _instanceLock = new object();
+        public static NoteCatalogue Instance
+        {
+            get
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance == null)  // lazy
+                    {
+                        _instance = new NoteCatalogue();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        /*
+         * Slut singleton
+         */
+        
+
+
+        private List<String> notes;
+
+        public List<string> Notes => new List<string>(notes);
+
+
+
+        public void Add(string item)
+        {
+            notes.Add(item);
+        }
+
+        public void Clear()
+        {
+            notes.Clear();
+        }
+
+
+        public override string ToString()
+        {
+            String strNotes = String.Join("\n", notes);
+            return $"Notes are :\n{strNotes}";
+        }
+    }
+}
